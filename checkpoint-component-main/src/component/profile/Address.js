b608f191655ada1a29f93d222c788c20e216a039
ahmed bchir <>
2022-02-02 10:12:44 +0100

    first commit

---
+++ checkpoint-component-main/src/component/profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Address = () => {
+  return <div>
+    <h2>Monastir C2 N78</h2>
+  </div>;
+};
+
+export default Address;
---
+++ checkpoint-component-main/src/component/profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import image from './113020297_10220492002147828_3300892163245301981_n.jpg';
+
+const ProfilePhoto = () => {
+  return <div>
+    <img src={image} alt='image'width={200}/>
+  </div>;
+};
+
+export default ProfilePhoto;
